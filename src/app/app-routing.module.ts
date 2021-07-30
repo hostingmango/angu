8a9ad89f84bf1d2685e970b1708d8c1aea458a5a
Ajit Pradhan <ajitpradhan925@gmail.com>
2021-07-30 21:27:12 +0530

    Create Account Successfully

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ThankYouComponent } from './thank-you/thank-you.component';
 
 
 const routes: Routes = [
@@ -14,6 +15,10 @@ const routes: Routes = [
     loadChildren: () =>
       import('./sign-in/sign-in.module').then((m) => m.SignInModule),
   },
+  {
+    path: 'thank-you/:email',
+    component: ThankYouComponent
+  }
 ];
 
 @NgModule({
